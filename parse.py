b36a8cc9419c9fa8670744974023ca58d29cd7eb
Farish-M <farishmohamedrozlan@gmail.com>
2026-06-05 04:26:34 +0800

    fix: moved code into a more fitting parse.py file name

--- parse.py
+++ parse.py
@@ -1,18 +1,11 @@
 import pandas as pd
 
-def clean_empty(data):
-    data.dropna(inplace = True)
-    return data
-
 def main():
     airport_data = "data/airports.dat"
     route_data = "data/routes.dat"
 
     airport_data = pd.read_csv(airport_data)
     route_data = pd.read_csv(route_data)
-
-    print(clean_empty(airport_data))
-    print(clean_empty(route_data))
 
 if __name__ == "__main__":
     main()

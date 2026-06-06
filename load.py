88e357953ef8fdc0f39d818b7be834be40054671
Farish-M <farishmohamedrozlan@gmail.com>
2026-06-06 11:28:49 +0800

    feat: load routes as relationships into neo4j

--- load.py
+++ load.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
 from parse import parse_airports
+from parse import parse_routes
 from dataclasses import asdict
 
 # read .env file
@@ -23,9 +24,21 @@ def load_airports(driver, rows):
         """
     driver.execute_query(query, airports=rows, database_="neo4j")
 
+def load_routes(driver, rows):
+    query = """
+        UNWIND $routes AS row
+        MATCH (s:Airport {iata: row.source_airport})
+        MATCH (d:Airport {iata: row.destination_airport})
+        MERGE (s)-[:FLYING_TO]->(d)
+        """
+    result = driver.execute_query(query, routes=rows, database_="neo4j")
+    print(f"Created {result.summary.counters.relationships_created} route edges")
+
 def main():
     airports = parse_airports("data/airports.dat")
-    rows = [asdict(a) for a in airports]
+    routes = parse_routes("data/routes.dat")
+    airport_rows = [asdict(a) for a in airports]
+    routes_rows = [asdict(a) for a in routes]
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
         print("Connected to Neo4j")
@@ -34,8 +47,10 @@ def main():
             "FOR (a:Airport) REQUIRE a.iata IS UNIQUE",
             database_="neo4j",
         )
-        load_airports(driver, rows)
-        print(f"Loaded {len(rows)} airports")
+        load_airports(driver, airport_rows)
+        print(f"Loaded {len(airport_rows)} airports")
+        load_routes(driver, routes_rows)
+        print(f"Loaded {len(routes_rows)} routes")
 
 if __name__ == "__main__":
     main()
